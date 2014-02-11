c6ea8fa0114310e6233f5838c4e0c6806bedcf08
tuxlinuxien <tuxlinuxien@gmail.com>
2014-02-11 21:06:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "TwistedWebsoket",
+    name = "TwistedWebsocket",
     version = "0.0.2",
     author = "Yoann Cerda",
     author_email = "tuxlinuxien@gmail.com",
     description = (""),
     license = "BSD",
-    keywords = "websocket server",
+    keywords = "websocket server twisted",
     url = "https://github.com/tuxlinuxien/TwistedWebsocket",
     packages=['TwistedWebsocket'],
     install_requires = ['twisted'],
